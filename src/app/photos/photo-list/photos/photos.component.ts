4427da7a2261197b462df98b277f71ceb28108b1
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-11 23:10:04 -0300

    created photos module

---
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+import { Photo } from "../../photo/photo";
+
+@Component({
+  selector: "app-photos",
+  templateUrl: "./photos.component.html",
+  styleUrls: ["./photos.component.css"],
+})
+export class PhotosComponent implements OnInit {
+  photos: Photo[] = [];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,10 +4,11 @@ import { PhotoComponent } from "./photo/photo.component";
 import { HttpClientModule } from "@angular/common/http";
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
   exports: [PhotoComponent],
 })
 export class PhotosModule {}
