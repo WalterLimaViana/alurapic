3a336496ab2e89602bb88cc34b71fe691feabb91
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-11 21:27:07 -0300

    created photo-list component

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PhotoComponent } from "./photo/photo.component";
 import { HttpClientModule } from "@angular/common/http";
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
-  declarations: [PhotoComponent],
+  declarations: [PhotoComponent, PhotoListComponent],
   exports: [PhotoComponent],
 })
 export class PhotosModule {}
