f5318b1f76a0f565d0f9eb1a4a14e36af30cc38a
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-12 20:57:33 -0300

    implementing a function to show the photos in 3 columns

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Photo } from "../../photo/photo";
 
 @Component({
@@ -7,9 +7,21 @@ import { Photo } from "../../photo/photo";
   styleUrls: ["./photos.component.css"],
 })
 export class PhotosComponent implements OnInit {
+  @Input()
   photos: Photo[] = [];
+  rows: any[] = [];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.rows = this.groupColumns(this.photos);
+  }
+  // A função groupColumns tem um slice que vai exibir as fotos de 3 em 3.
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+    return newRows;
+  }
 }
