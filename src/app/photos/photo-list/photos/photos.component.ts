e320ab460b5dc0031e4a702536809c52c7423db4
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-12 21:10:43 -0300

    inserting the Change Detection

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { Photo } from "../../photo/photo";
 
 @Component({
@@ -6,15 +6,20 @@ import { Photo } from "../../photo/photo";
   templateUrl: "./photos.component.html",
   styleUrls: ["./photos.component.css"],
 })
-export class PhotosComponent implements OnInit {
+
+// Para exibir as fotos após criar a função groupColumns, é necessário mudar o OnInit para OnChanges
+export class PhotosComponent implements OnChanges {
   @Input()
   photos: Photo[] = [];
   rows: any[] = [];
 
   constructor() {}
-
-  ngOnInit() {
-    this.rows = this.groupColumns(this.photos);
+  /*Esse if significa que se caso houver mudanças, e elas ocorrem devido a mudança do posicionamento das fotos
+  o angular a atraves do SimpleChanges trás essas fotos já com o novo posicionamento
+  
+  */
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photos) this.rows = this.groupColumns(this.photos);
   }
   // A função groupColumns tem um slice que vai exibir as fotos de 3 em 3.
   groupColumns(photos: Photo[]) {
