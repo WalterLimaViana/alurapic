e971acb9bf452014ed779a46fe194c0bc10bc11d
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-12 23:13:40 -0300

    using resolver to message error dissapear when photos is load

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,3 +1,4 @@
+import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
@@ -5,7 +6,13 @@ import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 
 const routes: Routes = [
-  { path: "user/:userName", component: PhotoListComponent },
+  {
+    path: "user/:userName",
+    component: PhotoListComponent,
+    resolve: {
+      photos: PhotoListResolver,
+    },
+  },
   { path: "p/add", component: PhotoFormComponent },
   { path: "**", component: NotFoundComponent },
 ];
