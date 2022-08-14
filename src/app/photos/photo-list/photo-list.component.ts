77fee05069d4fbb66410a94c7776f4a4c99047ca
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-13 23:43:02 -0300

    using operator debounceTime

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
 import { Photo } from "./../photo/photo";
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs/internal/Subject";
+import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: "app-photo-list",
@@ -10,10 +12,15 @@ import { ActivatedRoute } from "@angular/router";
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = "";
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data.photos;
+    //O debounce aguarda 300ms após o usuário terminar de digitar e assim colocar esse valor no filter para fazer a busca
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter) => (this.filter = filter));
   }
 }
