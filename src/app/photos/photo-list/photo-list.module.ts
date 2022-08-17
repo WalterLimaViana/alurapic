5969ee60310017357cd8622796c891a9db3f8ab3
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-16 21:13:59 -0300

    created darken-on-hover directive to change the brightness

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,7 +6,8 @@ import { PhotosComponent } from "./photos/photos.component";
 import { LoadButtonComponent } from "./load-button/load-button.component";
 import { PhotoModule } from "../photo/photo.module";
 import { FilterByDescription } from "./filter-by-description.pipe";
-import { SearchComponent } from './search/search.component';
+import { SearchComponent } from "./search/search.component";
+import { DarkenOnHoverModule } from "src/app/shared/directives/darken-on-hover/darken-onhover.module";
 
 @NgModule({
   declarations: [
@@ -16,6 +17,6 @@ import { SearchComponent } from './search/search.component';
     FilterByDescription,
     SearchComponent,
   ],
-  imports: [CommonModule, PhotoModule, CardModule],
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule],
 })
 export class PhotoListModule {}
