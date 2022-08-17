1919223a55d91a5015364a8b1618f4cc7dd46db5
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-17 20:57:49 -0300

    created validators to loginform

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   templateUrl: "./sigin.component.html",
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+  }
+}
