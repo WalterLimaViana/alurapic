ff58797411676a0ba60424f681de059cf7e507db
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-17 22:10:31 -0300

    created conection with API

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from "./../../core/auth.service";
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
@@ -7,11 +8,28 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ["", Validators.required],
       password: ["", Validators.required],
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get("userName").value;
+    const password = this.loginForm.get("password").value;
+
+    this.authService.authenticate(userName, password).subscribe(
+      () => console.log("autenticado"),
+      (err) => {
+        console.log(err);
+        this.loginForm.reset();
+        alert("User or password invalid!");
+      }
+    );
   }
 }
