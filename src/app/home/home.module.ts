4524347fbd399ae07a594498815d2db9bb9c6848
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-17 21:13:13 -0300

    update validators using a component to errors

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { SignInComponent } from "./signin/signin.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { VMessageComponent } from "../shared/components/card/vmessage/vmessage.component";
+import { VMessageModule } from "../shared/components/card/vmessage/vmessage.module";
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, VMessageModule],
 })
 export class HomeModule {}
