48b306ee87d9151890cdb2c63c99bf28d89cc25b
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-23 16:08:08 -0300

    created guard auth

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,9 +5,10 @@ import { RouterModule, Routes } from "@angular/router";
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { SignInComponent } from "./home/signin/signin.component";
+import { AuthGuard } from "./core/auth/auth.guard";
 
 const routes: Routes = [
-  { path: "", component: SignInComponent },
+  { path: "", component: SignInComponent, canActivate: [AuthGuard] },
   {
     path: "user/:userName",
     component: PhotoListComponent,
