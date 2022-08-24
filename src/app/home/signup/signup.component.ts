6b7f905a7378e478b99ff6d1365bff8d20433ffe
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-23 21:52:41 -0300

    created validators

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,3 +1,5 @@
+import { FormBuilder, Validators } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -6,7 +8,37 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./signup.component.css"],
 })
 export class SignUpComponent implements OnInit {
-  constructor() {}
+  signupForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      email: ["", [Validators.required, Validators.email]],
+      userName: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(40),
+        ],
+      ],
+      fullName: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-] +$/),
+          Validators.minLength(2),
+          Validators.maxLength(30),
+        ],
+      ],
+      password: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(14),
+        ],
+      ],
+    });
+  }
 }
