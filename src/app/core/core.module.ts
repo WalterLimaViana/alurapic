25317ffd6f6b30ca0ee5c558249f082198ae8bfa
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-25 22:16:04 -0300

    created a footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./header/header.component";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
