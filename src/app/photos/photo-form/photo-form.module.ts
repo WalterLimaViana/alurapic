8f515cff77a64ce1b1fca5952dac635a08cdb84c
WalterLimaViana <walter.lima.viana@gmail.com>
2022-08-25 22:21:59 -0300

    updated photo-form to upload and share photos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,11 @@
+import { VMessageModule } from "./../../shared/components/card/vmessage/vmessage.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { PhotoFormComponent } from "./photo-form.component";
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, VMessageModule, FormsModule],
 })
 export class PhotoFormModules {}
