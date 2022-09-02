95c4bd87a319f1d6dacc7b6c3397356c6812419c
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-01 23:30:29 -0300

    changed values

--- src/app/photos/photo/photo.service.spec.ts
+++ src/app/photos/photo/photo.service.spec.ts
@@ -1,15 +1,15 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
 
-import { PhotoService } from './photo.service';
+import { PhotoService } from "./photo.service";
 
-describe('PhotoService', () => {
+describe("PhotoService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PhotoService]
+      providers: [PhotoService],
     });
   });
 
-  it('should be created', inject([PhotoService], (service: PhotoService) => {
+  it("should be created", inject([PhotoService], (service: PhotoService) => {
     expect(service).toBeTruthy();
   }));
 });
