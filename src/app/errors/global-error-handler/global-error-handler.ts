01eecf88518bdbbe15363e5f6c28d241f3de572e
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-06 23:20:31 -0300

    created a serverlog

--- src/app/errors/global-error-handler/global-error-handler.ts
+++ src/app/errors/global-error-handler/global-error-handler.ts
@@ -1,3 +1,4 @@
+import { ServerLogService } from "./server-log.service";
 import { UserService } from "./../../core/user/user.service";
 import { LocationStrategy, PathLocationStrategy } from "@angular/common";
 import { ErrorHandler, Injectable, Injector } from "@angular/core";
@@ -12,21 +13,32 @@ export class GlobalErrorHandler implements ErrorHandler {
 
     const location = this.injector.get(LocationStrategy);
     const userService = this.injector.get(UserService);
+    const serverLogService = this.injector.get(ServerLogService);
 
     const url = location instanceof PathLocationStrategy ? location.path : "";
 
     const message = error.message ? error.message : error.toString();
+
     StackTrace.fromError(error).then((stackFrames) => {
       const stackAsString = stackFrames.map((sf) => sf.toString()).join("\n");
       console.log(message);
       console.log(stackAsString);
       console.log("o que será enviado para o servidor");
-      console.log({
-        message,
-        url,
-        userName: userService.getUserName,
-        stack: stackAsString,
-      });
+
+      serverLogService
+        .log({
+          message,
+          url,
+          userName: userService.getUserName(),
+          stack: stackAsString,
+        })
+        .subscribe(() => {
+          console.log("Error logged on server"),
+            (err) => {
+              console.log(err);
+              console.log("Fail to send error log to server");
+            };
+        });
     });
   }
 }
---
+++ src/app/errors/global-error-handler/server-log.ts
@@ -0,0 +1,6 @@
+export interface ServerLog {
+  message: string;
+  url: any;
+  userName: string;
+  stack: string;
+}
